b6a7511c75da212727274fae7993de36c6fe4377
pabloPc <pjorda96@gmail.com>
2021-02-20 12:58:31 +0100

    hof day 2

--- src/functions.js
+++ src/functions.js
@@ -170,6 +170,69 @@ function useFindDeep() {
   ))
 }
 
+function useReduce() {
+  const arr = [
+    { name: 'I', value: 7},
+    { name: 'aaI', value: 12},
+    { name: 'C', value: 1},
+    { name: 'D', value: 5},
+  ];
+  return arr.reduce((accumulator, currentValue) => ({
+    name: accumulator.name + currentValue.name,
+    value: accumulator.value + currentValue.value
+  }), { name: 'aa', value: 5})
+}
+
+function useReduceWithArr() {
+  const arr = [7, 3].concat([8, 0]);
+  return {
+    res: arr.reduce((accumulator, currentValue, i, arr) => {
+      console.log('i', i)
+      console.log(arr)
+
+      if (arr.indexOf(accumulator) > -1) {
+        console.log('iguales')
+      }
+
+      return accumulator + currentValue;
+    }, 1)
+  };
+}
+
+function useSortNumbers() {
+  const arr = [ 7, 10, 70, 55, 5, 1, 10, 0 ]
+
+  return arr.sort((a, b) => {
+
+    if (a > b) return 1
+    if (a === b) return 0
+    if (a < b) return -1
+  });
+}
+
+function useSortSubstrings() {
+  const arr = [ 'cccc', 'aaaa', 'bbbbbbbbbbbbbbb', 'aabb', 'bbaa', 'bbbb', 'ccaa', 'aaaaaaaaaaaaaaaaaaaaaaa' ]
+
+  return arr.sort((a, b) => {
+    if (a.substring(0, 2) > b.substring(0, 2)) return 1
+    if (a.substring(0, 2) === b.substring(0, 2)) return 0
+    if (a.substring(0, 2) < b.substring(0, 2)) return -1
+  });
+}
+
+function useKeys() {
+  const obj = {
+    javi: 'jabi',
+    eugenio: 'euhjenio',
+    pablo: { name: 'pavlo', surname: 'jorda' },
+  }
+
+  const keys = Object.keys(obj)
+  const res = Object.keys(obj).map(el => obj[el])
+
+  return { keys, res }
+}
+
 export default function hoFunctions() {
-  return useFindDeep();
+  return useKeys();
 }
